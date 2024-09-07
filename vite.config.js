9fe7ea4532b41c9c0e0a3335b2dbdb77f1aaac8a
ankonandi <ankonandi5002@gmail.com>
2024-09-07 16:47:29 +0530

    Basic navbar

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ankonandi.github.io/',
-})
+  server: {
+    mimeTypes: {
+      'text/jsx': 'application/javascript', 
+    },
+  },
+});
