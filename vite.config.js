68ace4d2b876bd57e173eba79cc698dab93156a0
ankonandi <ankonandi5002@gmail.com>
2024-09-07 17:59:43 +0530

    added navbar and animations

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ankonandi.github.io/',
-  server: {
-    mimeTypes: {
-      'text/jsx': 'application/javascript', 
-    },
-  },
-});
+  // base: '/ankonandi.github.io/',
+  // server: {
+  //   mimeTypes: {
+  //     'text/jsx': 'application/javascript', 
+  //   },
+  // },
+})
