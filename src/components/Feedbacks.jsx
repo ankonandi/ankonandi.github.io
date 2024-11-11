24d9fe3edc9bb0c653aedf83e07f4e55d53b3659
ankonandi <ankonandi5002@gmail.com>
2024-11-11 12:52:58 +0530

    added projects

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,19 +16,19 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
   >
-    <p className='text-white font-black text-[48px]'>"</p>
+    <p className="text-white font-black text-[48px]">"</p>
 
-    <div className='mt-1'>
-      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+    <div className="mt-1">
+      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
 
-      <div className='mt-7 flex justify-between items-center gap-1'>
-        <div className='flex-1 flex flex-col'>
-          <p className='text-white font-medium text-[16px]'>
-            <span className='blue-text-gradient'>@</span> {name}
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex-1 flex flex-col">
+          <p className="text-white font-medium text-[16px]">
+            <span className="blue-text-gradient">@</span> {name}
           </p>
-          <p className='mt-1 text-secondary text-[12px]'>
+          <p className="mt-1 text-secondary text-[12px]">
             {designation} of {company}
           </p>
         </div>
@@ -36,7 +36,7 @@ const FeedbackCard = ({
         <img
           src={image}
           alt={`feedback_by-${name}`}
-          className='w-10 h-10 rounded-full object-cover'
+          className="w-10 h-10 rounded-full object-cover"
         />
       </div>
     </div>
@@ -50,8 +50,43 @@ const Feedbacks = () => {
         className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
       >
         <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+          <a href="https://linktr.ee/ankonandi">
+            <h1 className={styles.sectionHeadText}>Projects</h1>
+            <p>
+              🔗 <span style={{ fontWeight: "bold" }}>Explore my work</span>
+            </p>
+          </a>
+
+          <a href="https://aadiaan.com/">
+            <h3 className={styles.sectionSubHeadText}>AADIAAN</h3>
+            <p className={styles.sectionSubText}>
+              We developed a mentorship platform designed to provide affordable
+              academic support to students from low- and middle-income groups.
+              Worked closely with developers and design teams to build an
+              intuitive user interface that promoted continuous learning.
+              Conducted user research to identify key challenges and aligned
+              product features with user needs, increasing engagement and
+              accessibility.
+            </p>
+            <p>
+              🔗 <span style={{ fontWeight: "bold" }}>Visit AADIAAN</span>
+            </p>
+          </a>
+
+          <a href="https://linktr.ee/nexwander">
+            <h3 className={styles.sectionSubHeadText}>Nexwander</h3>
+            <p className={styles.sectionSubText}>
+              Conceptualized and launched a travel platform connecting travelers
+              for group trips, ensuring safety and seamless service integration.
+              Led cross-functional teams to design features for rural tourism
+              and adventure travel, boosting user engagement. Focused on
+              creating solutions for both travelers and local service providers,
+              enhancing the overall travel experience.
+            </p>
+            <p>
+              🔗 <span style={{ fontWeight: "bold" }}>Explore Nexwander</span>
+            </p>
+          </a>
         </motion.div>
       </div>
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
